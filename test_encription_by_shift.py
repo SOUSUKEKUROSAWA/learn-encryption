f0d384e07cd36df1a4bc875dbb140171127fd371
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2023-09-23 18:12:02 +0900

    テストで使用する環境変数をモックするように修正

--- test_encription_by_shift.py
+++ test_encription_by_shift.py
@@ -1,11 +1,18 @@
 import unittest
+from unittest.mock import patch
 from encription_by_shift import encrypt, decrypt
 
 class TestEncryptionByShift(unittest.TestCase):
-    def test_that_encryption_can_be_performed_successfully_even_when_the_calculation_results_include_characters_with_numbers_smaller_than_the_start_of_the_character_code(self):
+
+    @patch('os.getenv')
+    def test_that_encryption_can_be_performed_successfully_even_when_the_calculation_results_include_characters_with_numbers_smaller_than_the_start_of_the_character_code(self, mock_getenv):
+        mock_getenv.return_value = '3'
+        
         info = "xyz"
         encrypted_info = encrypt(info)
         decrypted_info = decrypt(encrypted_info)
+        
+        self.assertEqual(info.upper(), decrypted_info)
 
 if __name__ == "__main__":
     unittest.main()
